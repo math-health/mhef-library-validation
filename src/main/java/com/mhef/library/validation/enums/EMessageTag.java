182bd85b30337fafdd311b55d4e39ac6fdc56662
Henrik Beck <henrikbeck95@gmail.com>
2023-11-12 20:21:44 -0300

    Closes #22

---
+++ src/main/java/com/mhef/library/validation/enums/EMessageTag.java
@@ -0,0 +1,15 @@
+package com.mhef.library.validation.enums;
+
+/**
+ * @author Henrik Beck
+ * @version 0.0.3
+ */
+public enum EMessageTag {
+	EMPTY,
+	ERROR,
+	NULL,
+	SUCCESS,
+	UNDEFINED,
+	UNKNOWN,
+	WARNING,
+}
